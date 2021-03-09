39a4e28455f2bce372d634bfa1a50cfde4f00897
filip <filis220@student.liu.se>
2021-03-09 09:38:02 +0100

    fixed Q learning

--- Qlearning.m
+++ Qlearning.m
@@ -1,7 +1,7 @@
 %% Initialization
 %  Initialize the world, Q-table, and hyperparameters
 
-world = 3;
+world = 4;
 state = gwinit(world);
 gwdraw();
 
@@ -13,10 +13,10 @@ action_size = length(actions);
 Q = -1 + 2*rand(state_size_y,state_size_x,action_size);
 
 %Hyperparameters
-episodes = 1000;
-epsilon = 0.1;  %   exploration factor
-learningRate = 0.5;
-gamma = 0.9;        %discount factor
+episodes = 2000;
+epsilon = 0.5;  %   exploration factor
+learningRate = 0.4;
+gamma = 0.8;        %discount factor
 prob_a = ones(1,length(actions))/length(actions); %uniform probability
 
 %% Training loop
@@ -30,6 +30,7 @@ for episode = 1:episodes
         
         state_y = state.pos(1);
         state_x = state.pos(2);
+        prev_state = gwstate(); %save previous state in case we want to go back
         [a, oa] = chooseaction(Q, state_y, state_x, actions, prob_a, epsilon);
         
         state = gwaction(a);
@@ -38,11 +39,20 @@ for episode = 1:episodes
         r = state.feedback;         %reward
         V = getvalue(Q);
         
-        Q(state_y,state_x, a) = (1-learningRate)*Q(state_y,state_x, a)...
+        
+       if state_y_next > state.ysize || state_y_next < 0 || state_x_next > state.xsize || state_x_next < 0  
+           r = -inf;
+           state = prev_state;
+           disp('----------------------')
+           disp(state_y_next)
+           disp(state_x_next)
+           disp(state)
+      end
+       
+       Q(state_y,state_x, a) = (1-learningRate)*Q(state_y,state_x, a)...
             + learningRate*(r + gamma*V(state_y_next,state_x_next));
-       if ~state.isvalid
-           Q(state_y,state_x, a) = -inf;
-       end
+       
+       
     end
 end
 
@@ -63,8 +73,6 @@ while state.isterminal~=1 %Run until agent reaches terminal
     [a, oa] = chooseaction(Q, state_y, state_x, actions, prob_a, 0);
 	state = gwaction(oa);
     gwdraw("Policy", getpolicy(Q))
-    pause(0.1)
-
 end 
 
 %P = getpolicy(Q);
